e0bb0a78f31b8f97eb8ff5ddfb30ccfc0593a06b
Siarhei Mahlysh <maglish@mail.ru>
2018-05-03 12:20:40 +0300

    Initial commit

---
+++ TrxParser/Models/TestRun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrxParser.Models
+{
+    [XmlRoot(ElementName = "TestRun", Namespace = "")]
+    public class TestRun
+    {
+        [XmlAttribute("id")]
+        public string RunId { get; set; }
+
+        [XmlAttribute("runUser")]
+        public string RunUser { get; set; }
+
+        [XmlElement("Times")]
+        public Times Times { get; set; }
+    }
+}
---
+++ TrxParser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TrxParser.Models;
+using TrxParser.Utils;
+
+namespace TrxParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string trxLocation = "TestResults.trx";
+    
+            TestRun testRun = TrxFileDeserializer.Deserialize<TestRun>(trxLocation);
+
+            Console.WriteLine(testRun);
+            Console.ReadKey();
+        }
+
+    }
+}
